c7489392205fcaa546bc2c6536f3a76cd01dedd5
조현아 <minjokwindows5@gmail.com>
2024-05-03 11:50:56 +0900

    Inital Project Settings

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //수강생 목록조회
+
+        //상태별 수강생 목록조회
+
+        //수강생 삭제
+
+        //특정 상태 수강생들의 필수 과목 평균 등급을 조회
+
+        //수강생 점수 목록 세팅 (점수 입력받음)
     }
 }
